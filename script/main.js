9dcd2e34c687e69eceb91269c2e2dc2ca14dd940
yube11 <yudha_11@apps.ipb.ac.id>
2022-06-30 11:13:27 +0700

    js new

--- script/main.js
+++ script/main.js
@@ -134,7 +134,7 @@ const animationTimeline = () => {
         rotationX: 15,
         rotationZ: -10,
         skewY: "-5deg",
-        y: 0,
+        y: 100,
         z: 10,
         opacity: 0,
       },
@@ -200,7 +200,7 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
+      x: -50,
       y: 350,
       rotation: -180,
       opacity: 0,
